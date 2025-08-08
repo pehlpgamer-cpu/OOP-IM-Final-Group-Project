db6b191f9815cacd44c418046365fb0f3d7fcaa4
GenZisyphus <no@email.com>
2025-08-09 07:42:05 +0800

    6th

--- OOP System Project/Auth_login.Designer.cs
+++ OOP System Project/Auth_login.Designer.cs
@@ -30,25 +30,38 @@ partial class Auth_login
     private void InitializeComponent()
     {
         txtBox_username = new System.Windows.Forms.TextBox();
+        txtBox_password = new System.Windows.Forms.TextBox();
         SuspendLayout();
         // 
         // txtBox_username
         // 
-        txtBox_username.Location = new System.Drawing.Point(235, 48);
+        txtBox_username.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        txtBox_username.Location = new System.Drawing.Point(127, 32);
         txtBox_username.Name = "txtBox_username";
-        txtBox_username.Size = new System.Drawing.Size(100, 27);
+        txtBox_username.Size = new System.Drawing.Size(171, 34);
         txtBox_username.TabIndex = 0;
+        // 
+        // txtBox_password
+        // 
+        txtBox_password.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        txtBox_password.Location = new System.Drawing.Point(127, 109);
+        txtBox_password.Name = "txtBox_password";
+        txtBox_password.Size = new System.Drawing.Size(171, 34);
+        txtBox_password.TabIndex = 1;
         // 
         // Auth_login
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(754, 450);
+        Controls.Add(txtBox_password);
         Controls.Add(txtBox_username);
         Text = "Login";
         ResumeLayout(false);
         PerformLayout();
     }
+
+    private System.Windows.Forms.TextBox txtBox_password;
 
     private System.Windows.Forms.TextBox txtBox_username;
 
